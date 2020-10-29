da9c9d15c67794cd05914512c1f9047084052327
edvardsk <edward2769940@gmail.com>
2020-10-29 18:46:49 +0300

    update babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,25 @@
 module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: 'commonjs',
+        targets: {
+          browsers: ['> 1%', 'last 2 versions'],
+        },
+        corejs: { version: 3, proposals: true },
+      },
+    ],
+    '@babel/react',
+  ],
+  plugins: [
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
+  ],
   env: {
     test: {
       presets: [['@babel/preset-env', { modules: 'commonjs' }], '@babel/preset-react'],
